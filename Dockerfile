d7a0b9a9ae28a01a0baf6aec5a948d3333b0235d
MohammadHasan Akbari <jarqvi.jarqvi@gmail.com>
2023-12-02 15:59:55 +0330

    refactor: update kibana version

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
-FROM bitnami/kibana:8.11.1
+FROM bitnami/kibana:8.11
 
 COPY lib/*.sh /docker-entrypoint-initdb.d/
--- push.sh
+++ push.sh
@@ -1,1 +1,1 @@
-docker push liaracloud/kibana-one-click-app:8.11.1
+docker push liaracloud/kibana-one-click-app:8.11
