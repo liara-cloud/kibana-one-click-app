facebda8b9e6fd12db6a936b62515591865835bd
MohammadHasan Akbari <jarqvi.jarqvi@gmail.com>
2023-12-03 14:37:24 +0330

    refactor: update node.options

--- lib/configure.sh
+++ lib/configure.sh
@@ -8,11 +8,11 @@ KIBANA_NODE_OPTIONS_PATH="/opt/bitnami/kibana/config/node.options"
 echo '> Configuring Kibana...'
 
 # https://www.elastic.co/guide/en/kibana/8.11/production.html#openssl-legacy-provider
-sed -i '/--dns-result-order=ipv4first/d' "$KIBANA_NODE_OPTIONS_PATH"
-sed -i '/## restore < Node 16 default DNS lookup behavior/d' "$KIBANA_NODE_OPTIONS_PATH"
+sed -i '/--openssl-legacy-provider/d' "$KIBANA_NODE_OPTIONS_PATH"
+sed -i '/## enable OpenSSL 3 legacy provider/d' "$KIBANA_NODE_OPTIONS_PATH"
 
 # https://www.elastic.co/guide/en/kibana/master/kibana-encryption-keys.html
-# /opt/bitnami/kibana/bin/kibana-encryption-keys generate --quiet >> "$KIBANA_CONFIG_PATH"
+/opt/bitnami/kibana/bin/kibana-encryption-keys generate --quiet >> "$KIBANA_CONFIG_PATH"
 
 echo "server.name: $KIBANA_SERVER_NAME" >> "$KIBANA_CONFIG_PATH"
 echo "server.publicBaseUrl: $KIBANA_SERVER_PUBLICBASEURL" >> "$KIBANA_CONFIG_PATH"
